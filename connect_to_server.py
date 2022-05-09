c4c6b32f2f6b6fc75f148cdd791e0515cc8bf088
Jurian Onderwater <jurian.onderwater247@gmail.com>
2022-05-09 16:50:50 +0200

    Add set_credentials for logging into server

--- connect_to_server.py
+++ connect_to_server.py
@@ -1,6 +1,14 @@
 import paramiko
 
-def connect(hostname, username, password):
+def set_credentials():
+    username = input("Username:")
+    password = input("Password:")
+    return [username, password]
+
+def connect(hostname: str, username: str, password: str):
+    """
+    Connects to a remote server using paramiko.
+    """
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname=hostname, username=username, password=password)
