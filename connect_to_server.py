71ac498d78afac5c25d153db7f9e44133cadb858
Jurian Onderwater <jurian.onderwater247@gmail.com>
2022-05-11 16:21:19 +0200

    Added documentation

--- connect_to_server.py
+++ connect_to_server.py
@@ -7,10 +7,21 @@ def set_credentials():
 
 def delete_credentials(username, password):
     username, password = None, None
+    pass
 
 def connect(hostname: str, username: str, password: str):
     """
     Connects to a remote server using paramiko.
+
+    --------
+    ### Arguments:
+    - hostname: str - The hostname of the server you are connecting to.
+    - username: str - The username you are using to log into the server.
+    - password: str - The password you are using to log into the server
+
+    --------
+    #### Returns:
+    - A paramiko SSHClient object that has been connected to.
     """
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
