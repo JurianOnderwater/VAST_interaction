b2b48dc893c33380414eff69ba28e714ebe836e6
Jurian Onderwater <jurian.onderwater247@gmail.com>
2022-05-11 16:21:04 +0200

    Updated documentation

--- file_send.py
+++ file_send.py
@@ -6,14 +6,17 @@ import paramiko
 class Send:
     """
     Transfers files to remote server.\n
-    Connects to remote server using SSH with the paramiko module for python.\n
+    Connects to remote server using SSH using the paramiko module for python.\n
     When initialising you need to pass a circularBuffer object as argument.
-    
+
     --------
-    # Functions:
-    - set_destination: Sets the path to where the files are transferred.
-    - print_destination:
-    - transfer_file:
+    ### Arguments:
+    - buffer: circularBuffer - This is a bufferobject used for dequeueing the files and send them
+    --------
+    ### Functions:
+    - set_destination(path: str): Sets the path to where the files are transferred.
+    - print_destination(): Prints the filepath to the destination folder on the server
+    - transfer_file():
     """
     def __init__(self, buffer: circularBuffer) -> None:
         self.buffer = buffer
@@ -26,24 +29,52 @@ class Send:
 
     def set_destination(self, path: str) -> None:
         """
-        Sets the path to where the files are transferred.
+        Sets the destination folder filepath
+
+        --------
+        Arguments:
+            path: str - filepath to the origin folder
+
+        --------
+        Raises:
+            TypeError - When no path is provided
         """
-        if (path is not None):
-            self.destination = path
+        if (path is None):
+            raise TypeError('You did not specify a pathname :(')
         else:
-            raise NameError('You did not specify a pathname :(')
+            self.destination = path
 
     def print_destination(self):
-        if (self.destination is not None):
-            print(self.destination)
-        else:
-            raise NameError('Oops, you did not yet set a destination folder!')
+        """
+        Prints the destination folder filepath.
 
-    def transfer_file(self, buffer: circularBuffer) -> None:
-        if (self.remote is not None):
-            file = buffer.dequeue()
-            self.ftp_client= self.ssh.open_sftp()
-            self.ftp_client.put(localpath=file,remotefilepath=self.destination)
-            print(f'Transferred {file} to {self.destination}')
-            self.ftp_client.close() 
-            os.remove(file)                                     
+        --------
+        Raises:
+            NameError - When path is not set yet
+        """
+        if (self.destination is None):
+            raise NameError('Oops, you did not yet set a destination folder!')            
+        else:
+            print(self.destination)
+
+    def transfer_file(self) -> None:
+        """
+        Transfers queued files to the remote server destination folder.\n
+        The file transfer protocol is opened, the file transferred, and the\n
+        transfer protocol closed again.\n 
+        The file is deleted from the origin folder.
+
+        --------
+        Raises:
+            NameError - When path is not set yet
+        """
+        if (self.remote is None):
+            raise NameError('The destination folder has not yet been set!')
+        else:
+            while self.buffer.size():
+                file = self.buffer.dequeue()
+                self.ftp_client= self.ssh.open_sftp()
+                self.ftp_client.put(localpath=file,remotefilepath=self.destination)
+                print(f'Transferred {file} to {self.destination}')
+                self.ftp_client.close() 
+                os.remove(file)                                     
