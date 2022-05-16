ea663805c62d2e04f241784c49f24ca1748b54ea
Jurian Onderwater <jurian.onderwater247@gmail.com>
2022-05-16 19:43:29 +0200

    modified test to have threading

--- scanner_test.py
+++ scanner_test.py
@@ -1,5 +1,19 @@
 from scan_folder import Scan
+from file_send import Send
+from connect_to_server import *
+from datastructures import circularBuffer
+import threading
+from threading import Thread, enumerate
 
-scanner = Scan()
-scanner.set_origin('test')
-scanner.scan_folder()
+
+if __name__ == '__main__':
+    buffer = circularBuffer()
+
+    scanner = Scan(buffer=buffer)
+    sender = Send(buffer=buffer)
+    scanner.set_origin('test')
+    sender.set_destination()
+    Thread(target = scanner.scan_folder()).start()
+    Thread(target = sender.transfer_files()).start()
+    for thread in threading.enumerate(): 
+        print(thread.name)
