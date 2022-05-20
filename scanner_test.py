20ddd387f71a194a2273999d956e6b6f112ba4c4
Jurian Onderwater <jurian.onderwater247@gmail.com>
2022-05-20 16:29:42 +0200

    working prototype

--- scanner_test.py
+++ scanner_test.py
@@ -2,18 +2,19 @@ from scan_folder import Scan
 from file_send import Send
 from connect_to_server import *
 from datastructures import circularBuffer
-import threading
-from threading import Thread, enumerate
+from threading import Thread
 
+def test():
+    print('venividivici')
 
 if __name__ == '__main__':
     buffer = circularBuffer()
 
     scanner = Scan(buffer=buffer)
-    sender = Send(buffer=buffer)
-    scanner.set_origin('test')
-    sender.set_destination()
-    Thread(target = scanner.scan_folder()).start()
-    Thread(target = sender.transfer_files()).start()
-    for thread in threading.enumerate(): 
-        print(thread.name)
+    sender = Send(buffer=buffer, testing=True)
+    scanner.set_origin(path='test')
+    sender.set_destination(path='/webhome/s2649438/public_html/test')
+    scanning_thread = Thread(target = scanner.scan_folder)
+    sending_thread  = Thread(target = sender.transfer_files)
+    scanning_thread.start()
+    sending_thread.start()
