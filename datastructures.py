16b0c9df182400f0046457e045ac69f64494da40
Jurian Onderwater <jurian.onderwater247@gmail.com>
2022-06-24 11:17:05 +0200

    implemented fifoBuffer

--- datastructures.py
+++ datastructures.py
@@ -1,3 +1,4 @@
+from tarfile import FIFOTYPE
 from time import sleep
 import os
 
@@ -8,13 +9,60 @@ class buffer:
         self.size = 0
         pass
 
-class circularBuffer(buffer):
+class fifoBuffer(buffer):
     """
-    circularBuffer implements an array with dynamic start - and endpoint.\n
+    fifoBuffer implements an array with dynamic start - and endpoint.\n
     The head and tail are updated when putting an item on the queue and\n 
     taking an item off of it. When the number of items in the queue grows\n
     bigger than max_size, the oldest item is taken out of the queue and the\n
     new item is placed on top.
+
+    --------
+    ### Arguments:
+    - `max_size (int)` - The maximum number of items in the buffer at any point in time.
+
+    --------
+    ### Functions:
+    - `dequeue()`: Takes the oldest item off of the queue.
+    - `enqueue(item)`: Puts item on top of the queue.
+    """
+    def __init__(self) -> None:
+        # self.max_size = max_size
+        # self.queue = [None] * max_size              # Make a list of max_size long
+
+        # self.second_chances = [0] * max_size        # Keep track of the second chances per item in the queue
+        self.tail = -1                              # Indicates where the newest item in the queue is
+        self.head = 0                               # Indicates where the olderst item in the queue is
+        # self.size = 0                               # Current size of the list  
+        self.network_speed = 20                     # Estimated speed of the connection  
+
+    def dequeue(self):
+        if self.size == 0:
+            print('The queue is empty')
+            return
+        else: 
+            tmp = self.queue[self.head]
+            self.head = (self.head + 1) % self.max_size
+        self.size -= 1
+        return tmp
+        
+    def enqueue(self, item):
+        
+        self.tail += 1
+        if self.size == self.max_size:
+            self.tail = (self.tail) % self.max_size
+            self.dequeue()
+        self.queue[self.tail] = item
+        self.size += 1
+
+class timedBuffer(buffer):
+    """
+    timedBuffer implements an array with dynamic start - and endpoint.\n
+    The head and tail are updated when putting an item on the queue and\n 
+    taking an item off of it. When the number of items in the queue grows\n
+    bigger than max_size, the oldest item is taken out of the queue and the\n
+    new item is placed on top. A delay is added to wait for a small amount of \n
+    time to let items that are almost done uploading finish uploading.
 
     --------
     ### Arguments:
@@ -74,7 +122,21 @@ class clockBuffer(buffer):
 
 class secondChanceBuffer(buffer):
     """
-    Some description
+    secondChanceBuffer implements an array with dynamic start - and endpoint.\n
+    The head and tail are updated when putting an item on the queue and\n 
+    taking an item off of it. When the number of items in the queue grows\n
+    bigger than max_size, the `enqueue()` looks for the oldest item that has \n
+    not been visited by the tail before and that item is taken out of the queue \n 
+    and the new item is placed on top.
+
+    --------
+    ### Arguments:
+    - `max_size (int)` - The maximum number of items in the buffer at any point in time.
+
+    --------
+    ### Functions:
+    - `dequeue()`: Takes the oldest item off of the queue.
+    - `enqueue(item)`: Puts item on top of the queue and checks which item should be taken off.
     """
     def __init__(self) -> None:
         self.visited = [0] * self.max_size
