bd175148d58cc33ba1075ecbee7c09c94a9c76ee
Jurian Onderwater <jurian.onderwater247@gmail.com>
2022-08-11 12:41:54 +0200

    moved GUI files to the right place

--- image_acquisition/signal_listener.py
+++ image_acquisition/signal_listener.py
@@ -4,23 +4,25 @@ ports = serial.tools.list_ports.comports(0)
 portList = []
 serialInst = serial.Serial()
 
+def listen():
+    for onePort in ports:
+        portList.append(str(onePort))
+        print(str(onePort))
 
-for onePort in ports:
-    portList.append(str(onePort))
-    print(str(onePort))
+    val = input("select port: /dev/cu.")
 
-val = input("select port: /dev/cu.")
+    for i in range(len(portList)):
+        if portList[i] .startswith('/dev/cu.' + str(val)):
+            portVar = "/dev/cu." + str(val)
+            print(portList[i])
 
-for i in range(len(portList)):
-    if portList[i] .startswith('/dev/cu.' + str(val)):
-        portVar = "/dev/cu." + str(val)
-        print(portList[i])
+    serialInst.port = portVar
+    serialInst.baudrate = 9600
+    serialInst.open()
 
-serialInst.port = portVar
-serialInst.baudrate = 9600
-serialInst.open()
+    while True:
+        if serialInst.in_waiting:
+            packet = serialInst.readline()
+            print(packet.decode('utf'))
 
-while True:
-    if serialInst.in_waiting:
-        packet = serialInst.readline()
-        print(packet.decode('utf'))
+listen()
