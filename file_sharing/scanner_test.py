a295fb3a3b4bf34d4c891852fa1a2aadd7eea326
jurianonderwater <jurian.onderwater247@gmail.com>
2022-10-13 11:28:06 +0200

    added ability to test acquire

--- file_sharing/scanner_test.py
+++ file_sharing/scanner_test.py
@@ -1,3 +1,4 @@
+from image_acquisition.micromanager import Acquire
 from scan_folder import Scan
 from file_send import Send
 from connect_to_server import *
@@ -5,15 +6,20 @@ from datastructures import *
 from threading import Thread
 
 
+
 if __name__ == '__main__':
+    path = r"\test"
+    name = " micromanager_acquisition"
     buffer = timedBuffer(max_size=20)
 
+    acquirer = Acquire(path=path, name=name)
     scanner = Scan(buffer=buffer)
     sender = Send(buffer=buffer, testing=True)
     scanner.set_origin(path='test')
     scanner.print_origin()
     sender.set_destination(path='/webhome/s2649438/public_html/test')
-    scanning_thread = Thread(target = scanner.scan_folder)
-    sending_thread  = Thread(target = sender.transfer_files)
+    acquisition_thread = Thread(target = acquirer.acquire())
+    scanning_thread    = Thread(target = scanner.scan_folder())
+    sending_thread     = Thread(target = sender.transfer_files())
     scanning_thread.start()
     sending_thread.start()
