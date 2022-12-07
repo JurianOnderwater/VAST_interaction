d9bd19a0ef60fbf49129e8ef3d52f151c8627ddf
Jurian Onderwater <jurian.onderwater247@gmail.com>
2022-12-07 11:17:52 +0100

    Added docstrign

--- image_acquisition/helper.py
+++ image_acquisition/helper.py
@@ -3,6 +3,16 @@ from matplotlib import pyplot as plt
 from PIL import Image, PngImagePlugin
 
 def rgba2rgb(rgba: np.array) -> np.array:
+    '''
+    Takes a numpy array and reshapes it into a width*height*channel shape.\n
+    If an alpha channel is present it is removed.
+    --------
+    ### Arguments:
+    - `rgba (np.array)` - pixel array.
+    --------
+    #### Returns:
+    - rgb array in the correct shape to plot it using pyplot.
+    '''
     row, col, ch = rgba.shape
 
     if ch == 3:
@@ -20,10 +30,23 @@ def rgba2rgb(rgba: np.array) -> np.array:
     return np.asarray(rgb, dtype='uint8')
 
 
-
 def image_process_fn(image: np.array, metadata: dict):
-    h, w, ch = 720, 1280, 4
-
+    '''
+    Saves images seperately instead of as a single large NDTIFF file.\n
+    metadata is encoded in the png file instead of as a seperate `dict`.\n
+    --------
+    ### Arguments:
+    Arguments are provided automatically when using in conjunction with the `acquire()` funtion
+    - `image (np.array)` - pixel array.
+    - `metadata` (dict) - metadata dictionary
+    --------
+    ### Returns:
+    Input is not altered when passing through this function and are returned unchanged.
+    - `original_image (np.array)` - The unchanged array of pixels.
+    - `metadata` - The unchanged metadata dictionary
+    '''
+    h, w, ch = 720, 1280, 4 #chech image dimensions. Probably take h and w from metadata.
+    original_image = image.copy
     image = image.reshape[h,w,ch]
     image = rgba2rgb(image)
     fig = plt.plot(image)
@@ -36,6 +59,6 @@ def image_process_fn(image: np.array, metadata: dict):
         meta.add_text(x, metadata[x])
     im.save('name', "png", pnginfo=meta)
 
-    return image, metadata
+    return original_image, metadata
 
 
