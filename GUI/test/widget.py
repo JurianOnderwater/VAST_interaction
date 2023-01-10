17d1522ca13b844fe30173ac166d01b4347f706b
Jurian Onderwater <jurian.onderwater247@gmail.com>
2023-01-10 11:00:00 +0100

    updated GUI

--- GUI/test/widget.py
+++ GUI/test/widget.py
@@ -5,45 +5,49 @@ from PySide6.QtWidgets import (
     QApplication,
     QCheckBox,
     QComboBox,
-    QDateEdit,
-    QDateTimeEdit,
-    QDial,
-    QDoubleSpinBox,
-    QFontComboBox,
     QLabel,
-    QLCDNumber,
     QLineEdit,
     QMainWindow,
-    QProgressBar,
     QPushButton,
-    QRadioButton,
     QSlider,
-    QSpinBox,
-    QTimeEdit,
     QVBoxLayout,
     QWidget,
 )
 
-from image_acquisition.micromanager import Acquire
+# from image_acquisition.micromanager import Acquire 
 
 
-# Subclass QMainWindow to customize your application's main window
+# Subclass QMainWindow to customize application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("VAST Acquisition")
 
         layout = QVBoxLayout()
 
-        self.Acquirer = Acquire()
-        self.magnificaitons_list = list(self.Acquirer.turret_dict.keys())
+        # self.Acquirer = Acquire()t
+        # self.magnifications_list = list(str(x) for x in self.Acquirer.turret_dict.keys())
+        self.magnifications_list = [10, 20, 2, 63, 4]
+        self.ticks = [50, 100, 150, 200]
+
         self.multi_channel = QCheckBox('Use Fluoresence')
+
         self.magnification_level = QComboBox()
-        self.magnification_level.addItems(self.magnificaitons_list)
+        self.magnification_level.addItems(str(x) for x in self.magnifications_list)
+        self.magnification_level.currentTextChanged.connect(self.magnification_value_change)
+
         self.test_label = QLabel('Acquisition name:')
         self.acquisition_name = QLineEdit()
+        # self.acquisition_name = QLineEdit(self.Aquirer.default_name)
+
         self.light_level = QSlider(Qt.Horizontal)
+        self.light_level.setMinimum(0)
+        self.light_level.setMaximum(255)
+        self.light_level.setTickPosition(QSlider.TicksAbove)
+        self.light_level.setTickInterval(50)
+        self.light_level.valueChanged.connect(self.light_value_change)
+
         self.start = QPushButton('Start')
         
         widgets = [
@@ -58,7 +62,7 @@ class MainWindow(QMainWindow):
         for w in widgets:
             layout.addWidget(w)
 
-        # layout.
+        # layout
         widget = QWidget()
         widget.setLayout(layout)
 
@@ -66,6 +70,13 @@ class MainWindow(QMainWindow):
         # to take up all the space in the window by default.
         self.setCentralWidget(widget)
 
+    def light_value_change(self):
+        brightness = self.light_level.value()
+        # self.Acquirer.set_optical_property(property='light level', value=brightness)
+
+    def magnification_value_change(self):
+        magnification = self.magnification_level.currentText()
+        # self.Acquirer.set_optical_property(property='zoom level', value=int(magnification))
 
 app = QApplication(sys.argv)
 window = MainWindow()
